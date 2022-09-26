82fea8b8800433c4b583f532485e1f5afc9d932c
jentevs <vansanjente@gmail.com>
2022-09-26 09:29:53 +0200

    scherm

--- main/main.py
+++ main/main.py
@@ -1,1 +1,30 @@
-print("hello")
+import pygame
+
+# intialize the pygame
+pygame.init()
+
+# create the screen
+screen = pygame.display.set_mode((1500, 800))
+
+#title and icon
+pygame.display.set_caption("Jumpysploinky")
+icon = pygame.image.load('NewPiskel1.png')
+pygame.display.set_icon(icon)
+
+
+#background
+
+run = True
+
+# loop
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    #background color
+    screen.fill((255,255,255))
+
+
+
+    pygame.display.update()
